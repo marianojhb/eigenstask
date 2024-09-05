fe58c42720d304fcdbabab7178264d91f03ad261
Mariano Belgrano <mbelgrano@gmail.com>
2024-09-05 14:41:20 -0300

    update

--- main.py
+++ main.py
@@ -1,19 +1,3 @@
-'''
-Siguiendo los documentos de 'fast api' trata de crearte lo siguiente:
-Entra a github.com, create una cuenta //  LISTO
-En la página de GitHub, en tu perfil, create un "repositorio" nombralo "eigentask"
-Hacerte un clon de ese repositorio en tu compu, y ahí create un servidor de fastapi básico,
-con dos rutas. //
-Una ruta que sea "/" a secas, y devuelva un JSON que sea {"hello": "world"} LISTO
-Otra ruta que sea "/random" y que devuelva un número aleatorio LISTO
--- hay muchas cosas ahí que capaz no conoces, googlealas, son bastante básicas en el laburo de
-programación y hay mucho texto online para hacerlo
--- la razón de la nomenclatura es que sea como una entrevista para entrar a eigen, y que tengas
-código público tuyo, que muestre que al menos sabes algo
--- la página de documentación de fast api':
-https://fastapi.tiangolo.com/tutorial/first-steps/
-'''
-
 from fastapi import FastAPI
 from random import randint
 
@@ -22,7 +6,12 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return 2
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
 
 @app.get("/random")
 async def random():
